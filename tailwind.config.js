6ca35a049088b5c0247fefc7550b1eddf1bd1f35
Luka Erić <lukaericba@gmail.com>
2024-09-04 20:27:30 +0200

    vercel fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors")
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,6 +15,7 @@ module.exports = {
       },
     },
       colors: {
+            ...colors,
             "border": "hsl(var(--border))",
             "input": "hsl(var(--input))",
             "ring": "hsl(var(--ring))",
