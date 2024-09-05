11dfdac7a9c35bd371fe8c7da8feb3104a90394e
Luka Erić <lukaericba@gmail.com>
2024-09-05 20:44:03 +0200

    profile functionality

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -25,14 +25,14 @@ const Profile = () => {
           </div>
         </div>
       ) : (
-        <div style={{width: '100%', height: '100%', padding: 10, background: 'white', borderRadius: 15, overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
+        <div style={{width: '100%', height: '100%', padding: 10, background: 'white', borderRadius: 15, overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: 15, display: 'inline-flex'}}>
           <img style={{width: 80, height: 80, position: 'relative', borderRadius: 999}} src="/F111.png" />
           <div style={{color: '#333333', fontSize: 22, fontFamily: 'Open Sans', fontWeight: '600', height: 26.40, wordWrap: 'break-word'}}>Overhelmed?</div>
-          <div style={{color: '#9CA3AF', fontSize: 16, fontFamily: 'Open Sans', fontWeight: '600', height: 19.20, wordWrap: 'break-word'}}>Personalize your vendor research</div>
-          <div style={{paddingLeft: 20, paddingRight: 20, paddingTop: 9, paddingBottom: 9, background: '#F3F3F3', borderRadius: 5, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', gap: 20, display: 'inline-flex'}}>
+          <div style={{color: '#9CA3AF', fontSize: 14, fontFamily: 'Open Sans', fontWeight: '400', height: 19.20, wordWrap: 'break-word'}}>Personalize your vendor research</div>
+          <button style={{paddingLeft: 20, paddingRight: 20, paddingTop: 9, paddingBottom: 9, background: '#F3F3F3', borderRadius: 5, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', gap: 20, display: 'flex', flexDirection: 'column'}}>
             <div style={{color: '#333333', fontSize: 14, fontFamily: 'Open Sans', fontWeight: '500', height: 16.80, wordWrap: 'break-word'}}>Build my vendor list for me</div>
+          </button>
           </div>
-        </div>
       )}
     </div>
   );
